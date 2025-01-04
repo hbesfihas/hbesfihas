6890ce4f006715c60ff9dfd34018de4887da5158
HB Esfihas Web Apps <contato.hbfarm@gmail.com>
2025-01-04 13:23:58 -0300

    Update urls.py

--- hbesfihas/delivery/urls.py
+++ hbesfihas/delivery/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LogoutView
-from . views import gerar_pix_qrcode_view
+from . views import gerar_pix_qrcode_view, criar_pedido
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.entrada_view, name='login'), 
     path('logout/', LogoutView.as_view(next_page='/login/'), name='logout'),
-    path('finalizar-pedido/', views.finalizar_pedido, name='finalizar_pedido'),
+    path('criar_pedido/', views.criar_pedido, name='criar_pedido'),
     path('pedidos/', views.pedidos, name='pedidos'),
     path('gerar_pix_qrcode/', gerar_pix_qrcode_view, name='gerar_pix_qrcode'),
-
+    path('contato', views.contato, name='contato')
 ]
